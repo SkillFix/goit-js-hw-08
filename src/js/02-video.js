2643d74a748efb404ee12b0624c4f63bbf88af68
SkillFix <107311906+SkillFix@users.noreply.github.com>
2022-09-13 16:54:45 +0300

    xtr

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,17 @@
+import Player from '@vimeo/player';
+let throttle = require('lodash.throttle');
 
+const player = document.querySelector('#vimeo-player');
+const videoPlayer = new Player(player);
+
+videoPlayer.on('timeupdate', throttle(savePlayedTime, 1000), { passive: true });
+
+function savePlayedTime() {
+  videoPlayer.getCurrentTime().then(function (seconds) {
+    localStorage.setItem('videoplayer-current-time', seconds);
+  });
+}
+
+videoPlayer.setCurrentTime(
+  localStorage.getItem('videoplayer-current-time') || 0
+);
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,37 @@
+let throttle = require('lodash.throttle');
 
+let feedback = document.querySelector('.feedback-form');
+
+feedback.addEventListener('input', throttle(saveData, 500));
+feedback.addEventListener('submit', submit);
+
+if (localStorage.getItem('feedback-form-state')) {
+  getData();
+}
+
+function saveData() {
+  let email = this.email.value;
+  let message = this.message.value;
+  localStorage.setItem(
+    'feedback-form-state',
+    JSON.stringify({ email, message })
+  );
+}
+
+function submit(event) {
+  event.preventDefault();
+  console.log(localStorage.getItem('feedback-form-state'));
+  localStorage.removeItem('feedback-form-state');
+  event.currentTarget.reset();
+}
+
+function getData() {
+  const data = JSON.parse(localStorage.getItem('feedback-form-state'));
+  try {
+    for (let key in data) {
+      feedback[key].value = data[key];
+    }
+  } catch (error) {
+    console.log('error', error);
+  }
+}
