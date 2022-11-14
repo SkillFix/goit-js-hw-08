ece7ac9ddde68f6ef5b1f75e85a8a3e4ea2f1ccd
SkillFix <107311906+SkillFix@users.noreply.github.com>
2022-11-14 18:00:56 +0200

    test

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,3 +1,34 @@
+// import picCardTpl from '../templaytes/pic-card.hbs';
+// import galleryItemTpl from '../templaytes/gallery-items.json';
+
+// import SimpleLightbox from 'simplelightbox';
+// import 'simplelightbox/dist/simple-lightbox.min.css';
+
+// // import { galleryItems } from './gallery-items.js';
+
+// const galleryContainerEl = document.querySelector('.gallery');
+// const galleryMarkup = createGalleryMarkup(galleryItemTpl);
+
+// galleryContainerEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
+// function createGalleryMarkup(galleryItemTpl) {
+//   return galleryItemTpl.map(picCardTpl).join('');
+// }
+
+// galleryContainerEl.addEventListener('click', openModal);
+
+// function openModal(event) {
+//   event.preventDefault();
+// }
+
+// const lightbox = new SimpleLightbox('.gallery a', {
+//   captionsData: 'alt',
+//   captionPosition: 'bottom',
+//   captionDelay: 100,
+// });
+
+// ===========================
+
 import SimpleLightbox from 'simplelightbox';
 import 'simplelightbox/dist/simple-lightbox.min.css';
 
