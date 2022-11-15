1b475a37b9c114b1935d266afbc908f995976b71
SkillFix <107311906+SkillFix@users.noreply.github.com>
2022-11-15 11:51:18 +0200

    final

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,54 +1,18 @@
-import picCardTpl from '../templaytes/pic-card.hbs';
-import galleryItemTpl from '../templaytes/gallery-items.json';
-
-import SimpleLightbox from 'simplelightbox';
-import 'simplelightbox/dist/simple-lightbox.min.css';
-
-// import { galleryItems } from './gallery-items.js';
-
-const galleryContainerEl = document.querySelector('.gallery');
-const galleryMarkup = createGalleryMarkup(galleryItemTpl);
-
-galleryContainerEl.insertAdjacentHTML('beforeend', galleryMarkup);
-
-function createGalleryMarkup(galleryItemTpl) {
-  return galleryItemTpl.map(picCardTpl).join('');
-}
-
-galleryContainerEl.addEventListener('click', openModal);
-
-function openModal(event) {
-  event.preventDefault();
-}
-
-const lightbox = new SimpleLightbox('.gallery a', {
-  captionsData: 'alt',
-  captionPosition: 'bottom',
-  captionDelay: 100,
-});
-
-// ===========================
+// import picCardTpl from '../templaytes/pic-card.hbs';
+// import galleryItemTpl from '../templaytes/gallery-items.json';
 
 // import SimpleLightbox from 'simplelightbox';
 // import 'simplelightbox/dist/simple-lightbox.min.css';
 
-// import { galleryItems } from './gallery-items.js';
+// // import { galleryItems } from './gallery-items.js';
 
 // const galleryContainerEl = document.querySelector('.gallery');
-// const galleryMarkup = createGalleryMarkup(galleryItems);
+// const galleryMarkup = createGalleryMarkup(galleryItemTpl);
 
 // galleryContainerEl.insertAdjacentHTML('beforeend', galleryMarkup);
 
-// function createGalleryMarkup(galleryItems) {
-//   return galleryItems
-//     .map(({ preview, original, description }) => {
-//       return `
-//      <a class="gallery__item" href="${original}">
-//       <img class="gallery__image" src="${preview}" alt="${description}" />
-//     </a>
-//     `;
-//     })
-//     .join('');
+// function createGalleryMarkup(galleryItemTpl) {
+//   return galleryItemTpl.map(picCardTpl).join('');
 // }
 
 // galleryContainerEl.addEventListener('click', openModal);
@@ -57,9 +21,45 @@ const lightbox = new SimpleLightbox('.gallery a', {
 //   event.preventDefault();
 // }
 
-// // Инициализация библиотеки
 // const lightbox = new SimpleLightbox('.gallery a', {
 //   captionsData: 'alt',
 //   captionPosition: 'bottom',
 //   captionDelay: 100,
 // });
+
+// ===========================
+
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { galleryItems } from './gallery-items.js';
+
+const galleryContainerEl = document.querySelector('.gallery');
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+galleryContainerEl.insertAdjacentHTML('beforeend', galleryMarkup);
+
+function createGalleryMarkup(galleryItems) {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return `
+     <a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>
+    `;
+    })
+    .join('');
+}
+
+galleryContainerEl.addEventListener('click', openModal);
+
+function openModal(event) {
+  event.preventDefault();
+}
+
+// Инициализация библиотеки
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 100,
+});
